4acebb6cb3a01b5d53aa63a002af26e66fee4491
fleischerdesign <philipp@fleischer.design>
2025-05-19 08:12:11 +0200

    Added hover animations to navigation.

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,15 +3,15 @@
         <div class="m-auto flex gap-2">
             <div
                 class="p-4 flex backdrop-blur-sm bg-gradient-to-br from-neutral-900/40 to-neutral-800/40 border rounded-lg border-neutral-700/40 hover:bg-gradient-to-br hover:from-neutral-800/40 hover:to-neutral-900/40 hover:border-neutral-600/40 hover:shadow-inner transition">
-                <Icon name="mdi:settings" size="30" class="opacity-60"/>
+                <Icon name="mdi:settings" size="30" class="opacity-60 hover:-translate-y-1 transition"/>
             </div>
             <div
                 class="flex gap-3 p-4 backdrop-blur-sm bg-gradient-to-br from-neutral-900/40 to-neutral-800/40 border rounded-lg border-neutral-700/40 hover:bg-gradient-to-br hover:from-neutral-800/40 hover:to-neutral-900/40 hover:border-neutral-600/40 hover:shadow-inner transition">
-                <Icon name="mdi:home-variant" size="30" />
-                <Icon name="mdi:person-circle" size="30" class="opacity-60"/>
-                <Icon name="mdi:text-box" size="30" class="opacity-60"/>
-                <Icon name="mdi:work" size="30" class="opacity-60"/>
-                <Icon name="mdi:store" size="30" class="opacity-60"/>
+                <Icon name="mdi:home-variant" size="30" class=""/>
+                <Icon name="mdi:person-circle" size="30" class="opacity-60 hover:-translate-y-1 transition"/>
+                <Icon name="mdi:text-box" size="30" class="opacity-60 hover:-translate-y-1 transition"/>
+                <Icon name="mdi:work" size="30" class="opacity-60 hover:-translate-y-1 transition"/>
+                <Icon name="mdi:store" size="30" class="opacity-60 hover:-translate-y-1 transition"/>
             </div>
         </div>
     </div>
