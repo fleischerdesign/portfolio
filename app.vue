935a0f223fea7399aa5c83f97617f97bdcd67fa9
fleischerdesign <philipp@fleischer.design>
2025-05-24 13:26:16 +0200

    changed primary icon set to mage.

--- app.vue
+++ app.vue
@@ -5,3 +5,18 @@
     </NuxtLayout>
   </div>
 </template>
+
+<style>
+html {
+  scroll-behavior: smooth;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.05s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,16 +2,32 @@
     <div class="flex fixed bottom-0 my-2 w-full">
         <div class="m-auto flex gap-2">
             <div
-                class="p-3 flex backdrop-blur-sm bg-gradient-to-br from-neutral-900/40 to-neutral-800/40 border rounded-lg border-neutral-700/40 hover:bg-gradient-to-br hover:from-neutral-800/40 hover:to-neutral-900/40 hover:border-neutral-600/40 hover:shadow-inner transition">
-                <Icon name="mdi:settings" size="30" class="opacity-60 hover:-translate-y-1 transition"/>
+                class="group p-3 flex backdrop-blur-md bg-gradient-to-br from-neutral-900/40 to-neutral-800/40 border rounded-lg border-neutral-700/40 hover:bg-gradient-to-br hover:from-neutral-800/40 hover:to-neutral-900/40 hover:border-neutral-600/40 hover:shadow-inner transition">
+                <Icon name="mage:bolt" size="30" class="opacity-60 hover:-translate-y-1 transition hover-group:opacity-0"/>
+                <Icon name="mage:bolt-fill" size="30" class="opacity-0 absolute transition group-hover:opacity-100"/>
             </div>
             <div
-                class="flex gap-3 p-3 backdrop-blur-sm bg-gradient-to-br from-neutral-900/40 to-neutral-800/40 border rounded-lg border-neutral-700/40 hover:bg-gradient-to-br hover:from-neutral-800/40 hover:to-neutral-900/40 hover:border-neutral-600/40 hover:shadow-inner transition">
-                <NuxtLink to="/" class="flex" exactActiveClass="*:!opacity-100 *:-translate-y-1"><Icon name="mdi:home-variant" size="30" class="opacity-40 hover:-translate-y-1 transition"/></NuxtLink>
-                <NuxtLink to="/about" class="flex" exactActiveClass="*:!opacity-100 *:-translate-y-1"><Icon name="mdi:person-circle" size="30" class="opacity-40 hover:-translate-y-1 transition"/></NuxtLink>
-                <NuxtLink to="/blog" class="flex" exactActiveClass="*:!opacity-100 *:-translate-y-1"><Icon name="mdi:text-box" size="30" class="opacity-40 hover:-translate-y-1 transition"/></NuxtLink>
-                <NuxtLink to="/projects" class="flex" exactActiveClass="*:!opacity-100 *:-translate-y-1"><Icon name="mdi:work" size="30" class="opacity-40 hover:-translate-y-1 transition"/></NuxtLink>
-                <NuxtLink to="/store" class="flex" exactActiveClass="*:!opacity-100 *:-translate-y-1"><Icon name="mdi:store" size="30" class="opacity-40 hover:-translate-y-1 transition"/></NuxtLink>
+                class="flex gap-3 p-3 backdrop-blur-md bg-gradient-to-br from-neutral-900/40 to-neutral-800/40 border rounded-lg border-neutral-700/40 hover:bg-gradient-to-br hover:from-neutral-800/40 hover:to-neutral-900/40 hover:border-neutral-600/40 hover:shadow-inner transition">
+                <NuxtLink to="/" class="group flex" exactActiveClass="active">
+                    <Icon name="mage:home" size="30" class="group-[.active]:opacity-0 opacity-40 transition group-hover:opacity-0"/>
+                    <Icon name="mage:home-fill" size="30" class="group-[.active]:opacity-100 absolute opacity-0 transition group-hover:opacity-40"/>
+                </NuxtLink>
+                <NuxtLink to="/about" class="group flex" exactActiveClass="active">
+                    <Icon name="mage:user-square" size="30" class="group-[.active]:opacity-0 opacity-40 transition group-hover:opacity-0"/>
+                    <Icon name="mage:user-square-fill" size="30" class="group-[.active]:opacity-100 absolute opacity-0 transition group-hover:opacity-40"/>
+                </NuxtLink>
+                <NuxtLink to="/blog" class="group flex" exactActiveClass="active">
+                    <Icon name="mage:note" size="30" class="group-[.active]:opacity-0 opacity-40 transition group-hover:opacity-0"/>
+                    <Icon name="mage:note-fill" size="30" class="group-[.active]:opacity-100 absolute opacity-0 transition group-hover:opacity-40"/>
+                </NuxtLink>
+                <NuxtLink to="/projects" class="group flex" exactActiveClass="active">
+                    <Icon name="mage:briefcase" size="30" class="group-[.active]:opacity-0 opacity-40 transition group-hover:opacity-0"/>
+                    <Icon name="mage:briefcase-fill" size="30" class="group-[.active]:opacity-100 absolute opacity-0 transition group-hover:opacity-40"/>
+                </NuxtLink>
+                <NuxtLink to="/store" class="group flex" exactActiveClass="active">
+                    <Icon name="mage:shop" size="30" class="group-[.active]:opacity-0 opacity-40 transition group-hover:opacity-0"/>
+                    <Icon name="mage:shop-fill" size="30" class="group-[.active]:opacity-100 absolute opacity-0 transition group-hover:opacity-40"/>
+                </NuxtLink>
             </div>
         </div>
     </div>
