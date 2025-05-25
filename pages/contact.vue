c041b10559c511883ca027d6f7ae8450badd037a
fleischerdesign <philipp@fleischer.design>
2025-05-25 15:10:12 +0200

    created contact page

---
+++ pages/contact.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container mx-auto py-16">
+        <h1 class="text-4xl font-bold mb-4">Contact</h1>
+        <p class="text-lg text-neutral-400 mb-8">
+        I’m an aspiring Computer Science Expert who thrives on clean code, seamless user experiences, and innovative
+        solutions that bring ideas to life.
+        </p>
+        <h2 class="text-2xl font-semibold mb-4">My Journey</h2>
+        <p class="text-lg text-neutral-400 mb-8">
+        My journey in the tech world began with a fascination for how things work. From building my first website to
+        developing complex applications, I have always been driven by a desire to learn and grow.
+        </p>
+        <h2 class="text-2xl font-semibold mb-4">Skills</h2>
+        <ul class="list-disc list-inside text-lg text-neutral-400 mb-8">
+        <li>Web Development</li>
+        <li>UI/UX Design</li>
+        <li>Problem Solving</li>
+        <li>Team Collaboration</li>
+        </ul>
+    </div>
+</template>
