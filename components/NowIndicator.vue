a71b797ae8a92a5e6f14b17e5dcc2383beb939ae
fleischerdesign <philipp@fleischer.design>
2025-05-29 13:10:53 +0200

    Put the nowIndicator into his own Component.

---
+++ components/NowIndicator.vue
@@ -0,0 +1,14 @@
+<template>
+    <Chip class="flex flex-row gap-7">
+        <div class="flex overflow-visible relative items-center justify-center drop-shadow-emit">
+            <Icon :name="now?.icon || 'mage:clock'" size="24" class="text-green-400 inset-0 absolute" />
+            <Icon :name="now?.icon || 'mage:clock'" size="24"
+                class="text-green-400 animate-ping origin-center inset-0 absolute" />
+        </div><span v-if="now?.status">{{ now?.status }}</span>
+    </Chip>
+</template>
+
+<script lang="ts" setup>
+const { locale } = useI18n();
+const now = await useFetch('/api/now', { query: { lang: locale.value } }).data
+</script>
