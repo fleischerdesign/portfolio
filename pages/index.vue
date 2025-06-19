f822fe9d97271edff3441ea86ec1f9174e10b0fa
fleischerdesign <philipp@fleischer.design>
2025-06-19 12:29:25 +0200

    fixed ogimage link on main site.

--- pages/index.vue
+++ pages/index.vue
@@ -88,14 +88,12 @@ useSeoMeta({
   ogTitle: t("navigation.home"),
   description: t("home.hero.summary"),
   ogDescription: t("home.hero.summary"),
-  ogImage: 'https://example.com/image.png',
   ogUrl: route.fullPath,
   ogType: 'website',
   ogLocale: locale.value,
   twitterTitle: t("navigation.home"),
   twitterCard: 'summary_large_image',
   twitterDescription: t("home.hero.summary"),
-  twitterImage: 'https://example.com/image.png',
   robots: 'index, follow',
 })
 
