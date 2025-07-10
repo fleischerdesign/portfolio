9aebae7dc0b19badb20cb1c6fd44a7a5ce0c2384
Philipp Fleischer <philipp@fleischer.design>
2025-07-10 20:03:59 +0200

    added ContactForm Component to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,18 +1,9 @@
 <template>
     <div class="container max-w-screen-xl mx-auto py-16">
     <HeadingSite :title="$t('home.contact.title')" :subtitle="$t('home.contact.subtitle')" />
-        <h2 class="text-2xl font-semibold mb-4">My Journey</h2>
-        <p class="text-lg text-neutral-400 mb-8">
-        My journey in the tech world began with a fascination for how things work. From building my first website to
-        developing complex applications, I have always been driven by a desire to learn and grow.
-        </p>
-        <h2 class="text-2xl font-semibold mb-4">Skills</h2>
-        <ul class="list-disc list-inside text-lg text-neutral-400 mb-8">
-        <li>Web Development</li>
-        <li>UI/UX Design</li>
-        <li>Problem Solving</li>
-        <li>Team Collaboration</li>
-        </ul>
+        <div class="mt-8">
+            <ContactForm />
+        </div>
     </div>
 </template>
 
