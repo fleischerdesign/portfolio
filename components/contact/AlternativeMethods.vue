1b02bb0d25dd7e5a8b9c1432aced10b78f5a1454
Philipp Fleischer <philipp@fleischer.design>
2025-07-12 18:50:38 +0200

    fixed contact data

--- components/contact/AlternativeMethods.vue
+++ components/contact/AlternativeMethods.vue
@@ -10,7 +10,7 @@
             <p class="text-neutral-400 mb-4">{{ $t('contact.email.description') }}</p>
             <div class="flex items-center gap-4 w-full px-4 py-3 shadow-sm bg-gradient-to-br dark:from-neutral-900 from-neutral-100 dark:to-neutral-800 to-neutral-200 border dark:border-neutral-700 border-neutral-300 rounded-lg">
                 <Icon name="mdi:email-outline" class="text-2xl text-neutral-400" />
-                <span class="text-lg">hello@fleischerdesign.de</span>
+                <span class="text-lg">hello@fleischer.design</span>
                 <Button @click="copyEmail" class="ml-auto" :aria-label="$t('contact.email.copy')">
                     <Icon :name="copyIcon" class="text-xl" />
                 </Button>
@@ -21,7 +21,7 @@
             <p class="text-neutral-400 mb-4">{{ $t('contact.phone.description') }}</p>
             <div class="flex items-center gap-4 w-full px-4 py-3 shadow-sm bg-gradient-to-br dark:from-neutral-900 from-neutral-100 dark:to-neutral-800 to-neutral-200 border dark:border-neutral-700 border-neutral-300 rounded-lg">
                 <Icon name="mdi:phone-outline" class="text-2xl text-neutral-400" />
-                <span class="text-lg">+49 123 456789</span>
+                <span class="text-lg">+49 176 31099324</span>
                 <Button @click="copyPhone" class="ml-auto" :aria-label="$t('contact.phone.copy')">
                     <Icon :name="phoneCopyIcon" class="text-xl" />
                 </Button>
@@ -35,7 +35,7 @@ const copyIcon = ref('mdi:content-copy');
 const phoneCopyIcon = ref('mdi:content-copy');
 
 const copyEmail = () => {
-    navigator.clipboard.writeText('mail@fleischerdesign.de');
+    navigator.clipboard.writeText('hello@fleischer.design');
     copyIcon.value = 'mdi:check';
     setTimeout(() => {
         copyIcon.value = 'mdi:content-copy';
@@ -43,7 +43,7 @@ const copyEmail = () => {
 };
 
 const copyPhone = () => {
-    navigator.clipboard.writeText('+49123456789');
+    navigator.clipboard.writeText('+4917631099324');
     phoneCopyIcon.value = 'mdi:check';
     setTimeout(() => {
         phoneCopyIcon.value = 'mdi:content-copy';
