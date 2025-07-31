1a58eed7c3818cc293fce1084f9cd8d40e5402fd
Philipp Fleischer <philipp@fleischer.design>
2025-07-31 22:45:21 +0200

    added resumeData.ts to the about me page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -260,6 +260,8 @@ sizes="100vw lg:400px" src="/img/business_atire.jpg"
 </template>
 
 <script lang="ts" setup>
+import { getResumeData } from '../data/resumeData.ts'
+
 const { t } = useI18n();
 const route = useRoute();
 const { locale } = useI18n();
@@ -278,69 +280,6 @@ useSeoMeta({
     robots: 'index, follow',
 })
 
-const careerTimeline = [
-    {
-        date: "2025 - 2027",
-        title: t("cv.timeline.wbs.title"),
-        description: t("cv.timeline.wbs.description"),
-        icon: "heroicons:academic-cap",
-        skills: ["Java", "Python", "C#", t("common.webdev"), "DevOps", t("common.databases")]
-    },
-    {
-        date: "2022 - 2025",
-        title: t("cv.timeline.ups.title"),
-        description: t("cv.timeline.ups.description"),
-        icon: "heroicons:briefcase",
-        skills: [t("common.teamleading"), t("common.process_optimization"), t("common.project_management"), t("common.warehouse_operations"), t("common.troubleshooting")]
-    },
-    {
-        date: "2020 - 2022",
-        title: t("cv.timeline.lidl.title"),
-        description: t("cv.timeline.lidl.description"),
-        icon: "heroicons:briefcase",
-        skills: [t("common.data_maintenance"), t("common.inventory_management"), t("common.retail_logistics"), t("common.customer_service"), t("common.process_optimization")]
-    },
-    {
-        date: "2017 - 2019",
-        title: t("cv.timeline.timeoff.title"),
-        description: t("cv.timeline.timeoff.description"),
-        icon: "mage:eye-closed",
-        skills: [t("common.self_improvement"), t("common.education"), t("common.analyze")]
-    },
-    {
-        date: "2015 - 2017",
-        title: t("cv.timeline.medienkolleg.title"),
-        description: t("cv.timeline.medienkolleg.description"),
-        icon: "heroicons:academic-cap",
-        skills: [t("common.audio_production"), t("common.video_production"), t("common.sound_design"), t("common.motion_graphics"), "Adobe Creative Cloud"]
-    },
-    {
-        date: "2014 - 2015",
-        title: t("cv.timeline.persch.title"),
-        description: t("cv.timeline.persch.description"),
-        icon: "heroicons:briefcase",
-        skills: ["Printdesign", t("common.digital_media"), t("common.graphic_design"), "Layoutdesign", "Brand Identity", "Typography", "Webdesign"]
-    },
-    {
-        date: "2012 - 2014",
-        title: t("cv.timeline.putbus.title"),
-        description: t("cv.timeline.putbus.description"),
-        icon: "heroicons:academic-cap",
-        skills: ["Printdesign", t("common.digital_media"), "Layoutdesign", "Typography", "Webdesign"]
-    },
-    {
-        date: "2007 - 2012",
-        title: t("cv.timeline.secschool.title"),
-        description: t("cv.timeline.secschool.description"),
-        icon: "heroicons:academic-cap",
-        skills: []
-    },
-    {
-        date: "2003 - 2007",
-        title: t("cv.timeline.elementary.title"),
-        description: t("cv.timeline.elementary.description"),
-        icon: "heroicons:academic-cap",
-        skills: []
-    }
-]
+const resumeData = getResumeData(t)
+const careerTimeline = resumeData.careerTimeline
 </script>
