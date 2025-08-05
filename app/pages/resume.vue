69e7ac3a68f11d2f901d9aaee6bc6775b4e54c9d
Philipp Fleischer <philipp@fleischer.design>
2025-08-05 14:53:19 +0200

    feat(resume): Redesign and update resume page
    - Restructured the resume page for better readability and a cleaner layout.
    - Added new sections for Interests, Languages, Courses, and Projects.
    - Updated the tech stack and career timeline.
    - Regenerated the resume PDF to reflect the latest changes.

--- app/pages/resume.vue
+++ app/pages/resume.vue
@@ -12,7 +12,7 @@
     <!-- Main Content Pages -->
     <div class="main-content-pages px-20 py-10">
       <div class="flex gap-2">
-      <HeadingSite
+      <UiHeading
         :title="$t('about.overview.resume.title')"
         :subtitle="''"
         symbol="logo:fleischerdesign"
@@ -46,14 +46,14 @@
             <h3 class="section-title text-primary-700 mb-4 border-l-4 border-secondary-400 pl-3 text-2xl font-bold">{{ $t('common.summary') }}</h3>
             <p class="text-base leading-relaxed">{{ resumeData.summary }}</p>
           </section>
+          <section class="mb-8">
+            <h3 class="section-title text-primary-700 mb-4 border-l-4 border-secondary-400 pl-3 text-2xl font-bold">{{ $t('home.overview.techstack.title') }}</h3>
+            <TechstackList :items="resumeData.techStack" :scroll="false" />
+          </section>
           <!-- Career Timeline Section -->
           <section class="mb-8">
             <h3 class="section-title text-primary-700 mb-4 border-l-4 border-secondary-400 pl-3 text-2xl font-bold">{{ $t('about.overview.careerPath.title') }}</h3>
             <BaseTimeline :items="resumeData.careerTimeline.filter(item => item.type === 'career')" :is-print-view="true" />
-          </section>
-          <section class="mb-8">
-            <h3 class="section-title text-primary-700 mb-4 border-l-4 border-secondary-400 pl-3 text-2xl font-bold">{{ $t('home.overview.techstack.title') }}</h3>
-            <TechstackList :items="resumeData.techStack" :scroll="false" />
           </section>
         </div>
       </div>
@@ -61,13 +61,41 @@
       <div class="main-content-pages break-inside-avoid-page px-20 py-10">
       <div class="flex gap-10">
         <div class="w-1/3">
-
+            <section class="mb-8">
+            <h3 class="section-title text-primary-700 mb-4 border-l-4 border-secondary-400 pl-3 text-2xl font-bold">Interessen</h3>
+            <ul>
+              <li><b>Technologie:</b> Programmieren, Maschinelles Lernen, Künstliche Intelligenz, Additive Fertigung, CAD, Videospiele</li>
+              <li><b>Kultur:</b> Bücher, Musik, Cuisine, Natur, Kunst, Design, Film</li>
+              <li><b>Gesundheit:</b> Laufen, Fitness, Mental, Ernährung</li>
+              <li><b>Bildung:</b> MINT, Psychologie, Geschichte, Politik, Biologie</li>
+            </ul>
+            </section>
+            <section class="mb-8">
+            <h3 class="section-title text-primary-700 mb-4 border-l-4 border-secondary-400 pl-3 text-2xl font-bold">Sprachen</h3>
+            <ul>
+              <li><b>Deutsch:</b> Muttersprachler</li>
+              <li><b>Englisch:</b> C1 Niveau</li>
+            </ul>
+            </section>
         </div>
         <div class="w-2/3">
           <section class="mb-8">
             <h3 class="section-title text-primary-700 mb-4 border-l-4 border-secondary-400 pl-3 text-2xl font-bold">Bildungsweg</h3>
             <BaseTimeline :items="resumeData.careerTimeline.filter(item => item.type === 'education')" :is-print-view="true" />
           </section>
+            <section class="mb-8">
+            <h3 class="section-title text-primary-700 mb-4 border-l-4 border-secondary-400 pl-3 text-2xl font-bold">Kurse</h3>
+            <TechstackList :items="resumeData.softSkills" :scroll="false" />
+            </section>
+            <section class="mb-8">
+            <h3 class="section-title text-primary-700 mb-4 border-l-4 border-secondary-400 pl-3 text-2xl font-bold">Projekte</h3>
+            <ul>
+              <li><b>Fleischer.design:</b> Bla</li>
+              <li><b>Ancoris:</b> Bla</li>
+              <li><b>Riffle:</b> Bla</li>
+              <li><b>Scriptorium:</b> Bla</li>
+            </ul>
+            </section>
         </div>
       </div>
       </div>
@@ -76,7 +104,6 @@
 
 <script lang="ts" setup>
 import { getResumeData } from '~/data/resumeData';
-import { useI18n } from '#imports';
 
 const { t } = useI18n();
 const resumeData = getResumeData(t);
