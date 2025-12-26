c1341b1af95af83db624d8287b0e02a0094f67c9
Philipp Fleischer <philipp@fleischer.design>
2025-12-27 00:14:25 +0100

    feat(ui): Refactor SectionHeader component
    This commit refactors the `SectionHeader.vue` component to align with the new UI component standards.
    
    - Refactored `SectionHeader.vue` to use the `useCva` composable for styling.
    - Added a new `align` prop (`'left' | 'center'`) to control the horizontal alignment of the header and its subtitle.
    - Implemented conditional styling for the subtitle's border based on the alignment for a better visual presentation.

--- app/components/ui/SectionHeader.vue
+++ app/components/ui/SectionHeader.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="mb-10 flex flex-col gap-4">
+  <div :class="rootClasses">
     <div class="flex items-center gap-2">
       <Icon v-if="symbol" :name="symbol" class="h-10 w-10 shrink-0 fill-secondary-400" mode="svg" />
       <component :is="tag" class="text-5xl font-semibold">{{ title }}</component>
     </div>
-    <p v-if="subtitle" class="w-full border-l-4 border-secondary-400 pl-4 text-neutral-600 lg:w-2/3 dark:text-neutral-400">
+    <p v-if="subtitle" :class="subtitleClasses">
       {{ subtitle }}
     </p>
   </div>
@@ -15,11 +15,35 @@ const props = withDefaults(defineProps<{
   title: string
   subtitle: string
   symbol?: string
-  level?: 1 | 2
+  level?: 1 | 2,
+  align?: 'left' | 'center'
 }>(), {
   level: 2,
   symbol: undefined,
+  align: 'left',
 })
 
 const tag = computed(() => `h${props.level}`)
+
+const rootClasses = useCva(
+  props,
+  'mb-10 flex flex-col gap-4',
+  {
+    align: {
+      left: 'items-start',
+      center: 'items-center text-center',
+    },
+  },
+);
+
+const subtitleClasses = useCva(
+  props,
+  'text-neutral-600 dark:text-neutral-400',
+  {
+    align: {
+      left: 'w-full border-l-4 border-secondary-400 pl-4 lg:w-2/3',
+      center: 'w-full border-l-0 border-t-4 border-secondary-400 pt-4',
+    },
+  },
+);
 </script>
