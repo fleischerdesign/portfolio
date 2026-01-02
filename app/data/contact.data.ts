c5b3e619f61362b47a47189a5caccf9e124b9ef5
Philipp Fleischer <philipp@fleischer.design>
2026-01-02 08:51:21 +0100

    fix(resume): changed the format of the mobile phone number

--- app/data/contact.data.ts
+++ app/data/contact.data.ts
@@ -1,6 +1,6 @@
 export const contactData = {
   email: "philipp@fleischer.design",
-  phone: "+49 176 310 99 324",
+  phone: "+49 176 31099324",
   website: "fleischer.design",
   github: "github.com/fleischerdesign",
   linkedin: "linkedin.com/in/fleischerdesign"
