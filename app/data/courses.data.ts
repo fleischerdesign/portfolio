7f26fe3f2da3ca29261a76cf8f909e281aa8f43f
Philipp Fleischer <philipp@fleischer.design>
2026-01-02 08:51:49 +0100

    feat(resume): added the From Nand to Tetris course

--- app/data/courses.data.ts
+++ app/data/courses.data.ts
@@ -1,13 +1,13 @@
 export const coursesData = [
   {
-    title: "Berufspraktisches Deutsch",
-    date: "23.05.2025 - 04.06.2025",
-    teacher: ["Ute Hegemann"]
+    title: "Build a Modern Computer from First Principles: From Nand to Tetris",
+    date: "12.12.2025 - 27.12.2025",
+    teacher: ["Hebrew University of Jerusalem"]
   },
   {
-    title: "Moderne Arbeitswelt: Kommunikation & Lernmethoden",
-    date: "07.06.2025 - 20.06.2025",
-    teacher: ["Peter Gerlach"]
+    title: "IT Basics",
+    date: "07.07.2025 - 01.08.2025",
+    teacher: ["Alexander Schwartz", "Lutz Galling", "Sina Cirillo", "Tobias Winkelhorst"]
   },
   {
     title: "Berufspraktisches Englisch",
@@ -15,9 +15,14 @@ export const coursesData = [
     teacher: ["Gwendolyn Mikschy"]
   },
   {
-    title: "IT Basics",
-    date: "07.07.2025 - 01.08.2025",
-    teacher: ["Alexander Schwartz", "Lutz Galling", "Sina Cirillo", "Tobias Winkelhorst"]
+    title: "Moderne Arbeitswelt: Kommunikation & Lernmethoden",
+    date: "07.06.2025 - 20.06.2025",
+    teacher: ["Peter Gerlach"]
+  },
+  {
+    title: "Berufspraktisches Deutsch",
+    date: "23.05.2025 - 04.06.2025",
+    teacher: ["Ute Hegemann"]
   },
   {
     title: "CAD and Digital Manufacturing",
